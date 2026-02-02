91f06c055b94e2c277199761bf488430e2263717
Arief <ariefrachmanmaulana@gmail.com>
2026-02-02 21:04:21 +0700

    commit-tc-login-negative

--- Test Suites/Login.ts
+++ Test Suites/Login.ts
@@ -11,4 +11,25 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>e868a7b4-53fb-4f2c-9c2d-928cde20d5f6</testSuiteGuid>
+   <testCaseLink>
+      <guid>ce3db42a-7760-4194-9a10-65a332c2887f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/Negative/User Login with Email and Password Empty</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>false</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8c1aa33c-5be2-46b7-a044-6afd2445e077</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/Negative/User Login with Invalid Email Format</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>false</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2d4f60dc-d026-4c60-9ec7-8088039d1ec0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/Negative/User Login with Password Empty</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>false</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
