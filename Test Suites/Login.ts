18d2a55ef4668f9255fff801bb4a8cb181ff7f09
Arief <ariefrachmanmaulana@gmail.com>
2026-02-02 20:49:50 +0700

    commit-delete-test-case

--- Test Suites/Login.ts
+++ Test Suites/Login.ts
@@ -11,39 +11,4 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>e868a7b4-53fb-4f2c-9c2d-928cde20d5f6</testSuiteGuid>
-   <testCaseLink>
-      <guid>4733e6bf-62f1-4290-be75-0671c4ea8421</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Login/Negative/User Login with Email and Password Empty</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>false</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>1f27ab06-2471-411f-954d-499f0cbe0f0a</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Login/Negative/User Login with Invalid Email Format</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>false</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>2f874c64-9427-4fd8-9f17-9b664c8a1a47</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Login/Negative/User Login with Password Empty</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>false</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>a8f4b34b-f454-4e73-9632-ef1943de28a4</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Login/Positive/User Login with Valid Email and Password</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>false</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>081a589f-d463-4e34-9114-ba24234e9b6b</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Login/Positive/User Open Login Page</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>false</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
 </TestSuiteEntity>
