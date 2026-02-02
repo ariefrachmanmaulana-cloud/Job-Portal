c18499af4fb1723fd2a9816885addb32caf4ffc9
testing <arief.rachman@finxpert.id>
2026-02-02 18:22:48 +0700

    commit-test-suites

--- Test Suites/Login.ts
+++ Test Suites/Login.ts
@@ -12,34 +12,6 @@
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>e868a7b4-53fb-4f2c-9c2d-928cde20d5f6</testSuiteGuid>
    <testCaseLink>
-      <guid>89a01738-c4f5-47a9-879f-ce8d78988a39</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Login/Negative/User Login with Email and Password Empty</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>false</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>90cee811-667f-4adb-b2a2-b8ea0eaf982e</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Login/Negative/User Login with Invalid Email Format</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>false</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>685c3e19-d978-4ea6-b17f-5c2df5f762a5</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Login/Negative/User Login with Password Empty</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>false</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>5f650724-66a1-4cc1-9095-5490de550475</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Login/Positive/User Login with Valid Email and Password</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>false</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
       <guid>4ede20e5-92a3-46da-ba98-36c22f149f10</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
